e54bfb37b7fa9950db41ef34e7914ac4ff1b87b5
shg4666 <shg3120591307@gmail.com>
2024-04-21 12:01:12 +0800

    1.I2C软件底层实现

--- STM32_Project/1-4 STM32_软件I2C/User/main.c
+++ STM32_Project/1-4 STM32_软件I2C/User/main.c
@@ -1,10 +1,8 @@
 #include "stm32f10x.h"                  // Device header
-#include "RED_GREEN_LED.h"
-#include "Encoder.h"
+#include "MyI2C.h"
 int main(void)
 {
-    RED_LED_Init();
-    Encoder_Init();
+    
     while(1)
     {
        
